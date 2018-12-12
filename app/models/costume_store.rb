e8e40fffd91eca5ff24b8f99c8abb680e7aead06
Jad Abdallah <justcallmethis@gmail.com>
2018-12-12 02:49:29 +0000

    Automatically backed up by Learn

--- app/models/costume_store.rb
+++ app/models/costume_store.rb
@@ -1,1 +1,11 @@
 # Create your CostumeStore class here
+
+class CostumeStore < ActiveRecord::Base 
+  create_table :costume_stores do |t| 
+      t.string :name 
+      t.string :location 
+      t.integer :inventory 
+      t.integer :employee_count
+      t.boolean :in_business
+      
+end
--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,13 @@
 # Create your costume_stores migration here
+
+class CreateCostumeStores < ActiveRecord::Migration[5.2]
+  
+  def change 
+    create_table :costumes do |t| 
+      t.string :name 
+      t.string :size 
+      t.string :image_url 
+      t.integer :price 
+  end 
+  
+end 
